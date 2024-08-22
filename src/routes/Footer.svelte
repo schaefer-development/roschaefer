8c78c896775da38f62d6e3188e4864bba2df8b4c
Robert Schäfer <git@roschaefer.de>
2024-08-22 15:43:11 +0200

    make github+linkedin links a component each

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,11 +1,15 @@
-<script>
-	import { page } from '$app/stores';
+<script lang="ts">
+	import GithubLink from '$lib/components/GithubLink.svelte';
+	import LinkedInLink from '$lib/components/LinkedInLink.svelte';
 </script>
 
 <footer
 	class="w-full bg-white py-6 flex flex-col md:flex-row justify-center items-center gap-y-1 md:gap-x-12 px-8"
 >
 	<a class="text-black max-w-max" href="mailto:hello@roschaefer.de">hello@roschaefer.de</a>
-	<a class="text-black max-w-max" href="callto:004923">+49 (0)170 - 12 34 456</a>
+	<div class="flex gap-6">
+		<LinkedInLink fill="black"></LinkedInLink>
+		<GithubLink fill="black"></GithubLink>
+	</div>
 	<a class="max-w-max" href="/imprint">imprint & data privacy</a>
 </footer>
