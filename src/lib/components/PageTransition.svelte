19922356b21b6d385e7f9d7617c81dc428204ff7
joschaefer <mail@joschaefer.com>
2024-08-22 10:42:18 +0200

    feat: add basic folders and components

---
+++ src/lib/components/PageTransition.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	export let url = URL;
+</script>
+
+{#key url}
+	<div
+		in:fade={{ duration: 300, delay: 400 }}
+		class="top-0 left-0 min-h-screen h-full w-full bg-black"
+	>
+		<slot />
+	</div>
+{/key}
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<footer class="bg-white py-6 flex gap-x-4 justify-center">
+	<a href="mailto:hello@roschaefer.de">hello@roschaefer.de</a>
+	<a href="callto:004923">+49 (0)170 - 12 34 456</a>
+	<a href="/imprint">imprint / data privacy</a>
+</footer>
