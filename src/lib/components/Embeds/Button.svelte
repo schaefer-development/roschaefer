7ba0bfdefeacb017d912b6a9254e348f5a060a06
Robert Schäfer <git@roschaefer.de>
2024-09-22 19:08:01 +0200

    better icon

--- src/lib/components/Embeds/Button.svelte
+++ src/lib/components/Embeds/Button.svelte
@@ -3,30 +3,12 @@
 </script>
 
 <button
-	class="w-full aspect-video top-0 left-0 flex flex-col justify-center items-center text-white hover:bg-black hover:bg-opacity-30 transition ease-in-out duration-250"
+	class="w-full aspect-video top-0 left-0 flex flex-col justify-center items-center text-white hover:bg-black hover:bg-opacity-30 transition ease-in-out duration-500"
 	on:click
 	aria-label="Play Facebook video"
 >
-	<svg
-		class="h-20 w-20 fill-white"
-		version="1.1"
-		id="Capa_1"
-		xmlns="http://www.w3.org/2000/svg"
-		xmlns:xlink="http://www.w3.org/1999/xlink"
-		viewBox="0 0 60 60"
-		xml:space="preserve"
-	>
-		<g>
-			<path
-				d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30
-      c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15
-      C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z"
-			/>
-			<path
-				d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30
-      S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z"
-			/>
-		</g>
-	</svg>
+  <svg class="h-20 w-20 fill-transparent stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" >
+    <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+  </svg>
 	<p>{explanation}</p>
 </button>
