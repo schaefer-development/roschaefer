bbd7fd6cb484d94d71d2f6b63dff6c1b2809be87
Robert Schäfer <git@roschaefer.de>
2024-11-11 17:32:20 +0100

    add tailwind typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
@@ -40,5 +41,5 @@ export default {
 		}
 	},
 
-	plugins: []
+	plugins: [typography]
 } satisfies Config;
