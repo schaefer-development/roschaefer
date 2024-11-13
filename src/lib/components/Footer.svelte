91993b94f866b478e28e2be6be53cedb71f408f0
joschaefer <mail@joschaefer.com>
2024-11-13 19:59:07 +0100

    feat: styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,7 +6,7 @@
 <footer
 	class="flex w-full flex-col items-center justify-center gap-y-1 bg-black px-8 py-12 text-sm uppercase tracking-widest md:flex-row md:gap-x-12"
 >
-	<EmailLink classNames=" max-w-max"></EmailLink>
-	<ProfileList fill="black"></ProfileList>
-	<a class="max-w-max" href="/imprint">imprint & data privacy</a>
+	<EmailLink classNames="text-white max-w-max"></EmailLink>
+	<ProfileList></ProfileList>
+	<a class="max-w-max text-white" href="/imprint">imprint & data privacy</a>
 </footer>
